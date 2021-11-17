c316b1a092bc0ab8c93f956f1684ae4ad15e7d9e
Marius Captari <m.c.captari@student.rug.nl>
2021-11-17 15:29:59 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Marius Captari on 17/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
